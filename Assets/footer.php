c9e8e81846008da82260c5697a03d79a9b5d3082
Sander Weide <sanderweide11@gmail.com>
2019-05-03 15:59:49 +0200

    footer en css van footer zijn toegevoegd

---
+++ Assets/footer.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Naamloos document</title>
+<link href="stylezb.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+	<div class="footer">
+		<div class="footerTop"><h2>Links</h2>
+			<div class="footerDivider"></div>
+			<p>Melden<br>
+				Sitemap</p></div>
+		<div class="footerTop"><h2>Contact</h2>
+			<div class="footerDivider"></div>
+			<p>info@tweedehandsfiets.nl<br>
+				+31 (0) 123 456 789<br>
+				Fietsengracht 12<br>
+				1234 AB, Amsterdam<br>
+			</p></div>
+		<div class="footerTop"><h2>Wordt lid van de nieuwsbrief</h2>
+			<div class="footerTopEmail">
+				<form>
+					<table>
+						<tr><td><input type="email" placeholder="Email adress"></td></tr>
+						<tr><td><input type="submit" value="Schrijf in"></td></tr>
+					</table>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="footerBottom">
+		<h2>© 2019 - Tweedehands Fiets, alle rechten voorbehouden</h2>
+	</div>
+</body>
+</html>
