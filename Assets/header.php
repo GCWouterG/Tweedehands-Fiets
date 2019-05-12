dfa56074db21c2d67ef82cef5fe50dbb1b7c7137
Woutuhr <47321566+Woutuhr@users.noreply.github.com>
2019-05-12 21:51:32 +0200

    Synchronisatie commit

--- Assets/header.php
+++ Assets/header.php
@@ -11,7 +11,7 @@
 	
 	<title><?php echo $pageTitle;?> | Tweedehands Fiets B.V.</title>
 	<link rel="stylesheet" type="text/css" href="Assets/style.css">
-	<link rel="icon" href="images/favicon.png">
+	<link rel="icon" href="Assets/images/favicon.png">
 	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
--- index.php
+++ index.php
@@ -70,20 +70,40 @@
 	
 	</div>
 	
-	<div class="row">
+	<div class="row" id="homepageContent">
 		<div class="col-8">
 			<div class="row" id="actions">
 				<h1>Acties van de dag</h1>
-				<div class="adWrapper">
-					<div class="ad">
+				<div id="homepageBikesWrapper">
+					<div class="homepageBike">
 						<img src="Assets/images/Uploads/bike.png" alt="Bike">
 						<p>Lorem Ipsum dolor si di amet</p>
+						<h4 class="homepageBikeOriginalPrice">&euro; 123,45</h4><h4 class="homepageBikeActionPrice">&euro; 123,45</h4>
+						<a href="">Bekijk</a>
+					</div>
+				</div>
+				<div id="homepageBikesWrapper">
+					<div class="homepageBike">
+						<img src="Assets/images/Uploads/bike.png" alt="Bike">
+						<p>Lorem Ipsum dolor si di amet</p>
+						<h4 class="homepageBikeOriginalPrice">&euro; 123,45</h4><h4 class="homepageBikeActionPrice">&euro; 123,45</h4>
+						<a href="">Bekijk</a>
+					</div>
+				</div>
+				<div id="homepageBikesWrapper">
+				<div id="homepageBikesWrapper">
+					<div class="homepageBike">
+						<img src="Assets/images/Uploads/bike.png" alt="Bike">
+						<p>Lorem Ipsum dolor si di amet</p>
+						<h4 class="homepageBikeOriginalPrice">&euro; 123,45</h4><h4 class="homepageBikeActionPrice">&euro; 123,45</h4>
+						<a href="">Bekijk</a>
 					</div>
 				</div>
 			</div>
 			<div class="row" id="bikes">
 				<h1>Nieuwste fietsen</h1>
 			</div>
+		</div>
 		</div>
 		<div class="col-4">
 			<div class="row" id="category">
@@ -96,11 +116,6 @@
 	</div>
 
 </div>
-
-	
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<?php require('Assets/footer.php');?>
 </body>
 </html>
