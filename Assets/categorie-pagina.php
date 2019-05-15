c6df96b932316b8a723ca6e9ad3e4e4bde17563d
LisaDiks <47321976+LisaDiks@users.noreply.github.com>
2019-05-15 09:05:54 +0200

    categorie-page
    wel af, Nog niet responsive

---
+++ Assets/categorie-pagina.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Naamloos document</title>
+	<link href="categorie-opmaak.css" type="text/css" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+	<?php
+		require 'header.php';
+	?>
+	<br>
+	<h1 id="h1">Mannenfietsen</h1>
+	<hr id="hr">
+	
+	<table class="overzicht">
+		<tr>
+			<td class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></td>
+			<td class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></td>
+			<td class="prijs-td"> €123,45 </td>
+			<td class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </td>
+		</tr>
+		<tr>
+			<th class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></th>
+			<th class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></th>
+			<th class="prijs-td"> €123,45 </th>
+			<th class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </th>
+		</tr>
+		<tr>
+			<td class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></td>
+			<td class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></td>
+			<td class="prijs-td"> €123,45 </td>
+			<td class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </td>
+		</tr>
+		<tr>
+			<th class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></th>
+			<th class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></th>
+			<th class="prijs-td"> €123,45 </th>
+			<th class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </th>
+		</tr>
+		<tr>
+			<td class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></td>
+			<td class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></td>
+			<td class="prijs-td"> €123,45 </td>
+			<td class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </td>
+		</tr>
+		<tr>
+			<th class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></th>
+			<th class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></th>
+			<th class="prijs-td"> €123,45 </th>
+			<th class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </th>
+		</tr>
+		<tr>
+			<td class="img-td"> <img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></td>
+			<td class="tekst-td"> Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></td>
+			<td class="prijs-td"> €123,45 </td>
+			<td class="button-td"> <a href="#" class="btn btn-primary"> Bekijk </a> </td>
+		</tr>
+		
+	</table>
+	
+
+	<br>
+	
+	<?php
+	 
+	require 'footer.php';
+	
+	?>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
--- Assets/header.php
+++ Assets/header.php
@@ -2,7 +2,7 @@
 	session_start();
 	
 	$pageTitle = "{title}";
-	$_SESSION['loggedIn'] = true;
+	$_SESSION['loggedIn'] = false;
 ?>
 <!doctype html>
 <html>
@@ -12,11 +12,11 @@
 	<meta name="Description" content="Tweedehands fietsensite - SCRUM project">
 	<meta name="Keywords" content="tweedehands, fiets, tweedehands fiets, fietsen, goedkoop">
 	
-	<title><?php echo $pageTitle;?> | Tweedehands Fiets B.V.</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	<title><?php echo $pageTitle;?> | Tweedehands Fiets B.V.</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
 <body>
