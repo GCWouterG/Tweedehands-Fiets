bc2a90657478663943e9e7a5cd6ad2718a15dba2
yoteth <47322123+yoteth@users.noreply.github.com>
2019-05-15 11:31:45 +0200

    Laatste code geordend

--- Assets/footer.php
+++ Assets/footer.php
@@ -1,4 +1,5 @@
 	<footer id="upperFooter">
+		
 		<div class="row">
 			<div class="col" id="footerLinks">
 				<h2>Links</h2>
@@ -33,14 +34,19 @@
 			</div>
 			</div>
 		</div>
+
 	</footer>
+
 	<footer id="lowerFooter">
+		
 		<span>&copy; <?php echo date("Y");?> - Tweedehands Fiets, alle rechten voorbehouden</span>
+		
 	</footer>
 	
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	<script src="Assets/script.js"></script>
+
 </body>
 </html>
