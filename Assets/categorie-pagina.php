bd724b31fd23e45b360187cd5c47516ce5d6eb29
Wouter G <47321566+Woutuhr@users.noreply.github.com>
2019-05-17 11:50:28 +0200

    kleine aanpassing gemaakt

--- Assets/categorie-pagina.php
+++ Assets/categorie-pagina.php
@@ -37,41 +37,10 @@
 		<div class="prijs-td">&euro;<?php echo str_replace(".", ",", $row['fietsPrijs']); ?></div>
 		<div class="button-td"><a href="bike.php?id=<?php echo $row['fietsID']?>" class="btn btn-primary"> Bekijk </a></div>
 	</div>
-	<?php } ?>
-<!--
-	<div class="categorie-row">
-		<div class="img-td"><img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></div>
-		<div class="tekst-td">Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></div>
-		<div class="prijs-td">€123,45</div>
-		<div class="button-td"><a href="#" class="btn btn-primary"> Bekijk </a></div>
-	</div>
-	<div class="categorie-row">
-		<div class="img-td"><img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></div>
-		<div class="tekst-td">Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></div>
-		<div class="prijs-td">€123,45</div>
-		<div class="button-td"><a href="#" class="btn btn-primary"> Bekijk </a></div>
-	</div>
-	<div class="categorie-row">
-		<div class="img-td"><img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></div>
-		<div class="tekst-td">Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></div>
-		<div class="prijs-td">€123,45</div>
-		<div class="button-td"><a href="#" class="btn btn-primary"> Bekijk </a></div>
-	</div>
-	<div class="categorie-row">
-		<div class="img-td"><img src="images/Elektrische+fiets+E+bike+Elops+920+hoog+frame+stadsfiets+donkergroen.jpg"></div>
-		<div class="tekst-td">Lorem Ipsum dolor si di amet <br> <p>Arnhem, GE</p></div>
-		<div class="prijs-td">€123,45</div>
-		<div class="button-td"><a href="#" class="btn btn-primary"> Bekijk </a></div>
-	</div>
--->
-	
-
-	<br>
-	
 	<?php
-	 
-	require 'footer.php';
+		}
 	
+		require 'footer.php';
 	?>
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
