8b9dd18c4f40bd7688054cafe8012938d25c386a
Wouter G <47321566+Woutuhr@users.noreply.github.com>
2019-05-22 12:53:45 +0200

    Update connect.php

--- Assets/connect.php
+++ Assets/connect.php
@@ -1,13 +1,14 @@
 <?php
 	$dbHost = "localhost";
-	$dbUser = "root";
-	$dbPass = "";
-	$dbName = "tweedehands_fiets";
+	$dbUser = "wgosseling_tweedehandsfiets";
+	$dbPass = "Scrum01!";
+	$dbName = "wgosseling_tweedehands_fiets";
 	
 	$conn = new Mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
 	if($conn->connect_error) {
 		$_SESSION['databaseError'] = true;
-//		header("Location: index.php");
+		//header("Location: index.php");
+		echo "Fout";
 	}
 ?>
