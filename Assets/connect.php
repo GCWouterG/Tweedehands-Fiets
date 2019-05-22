2f6fe57561239bbd7026014e9e4e57ce52e2422c
Wouter G <47321566+Woutuhr@users.noreply.github.com>
2019-05-22 10:50:25 +0200

    Sync commit

--- Assets/connect.php
+++ Assets/connect.php
@@ -1,8 +1,8 @@
 <?php
-	$dbHost = "185.87.187.247";
-	$dbUser = "wgosseling_tweedehandsfiets";
-	$dbPass = "Scrum01!";
-	$dbName = "wgosseling_tweedehands_fiets";
+	$dbHost = "localhost";
+	$dbUser = "root";
+	$dbPass = "";
+	$dbName = "tweedehands_fiets";
 	
 	$conn = new Mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
--- bike.php
+++ bike.php
@@ -2,27 +2,36 @@
 	$pageTitle = "Fiets";
 	require('Assets/header.php');
 
-	$getBikeInfo = "SELECT *
-					FROM fietsen
-					INNER JOIN staat
-					ON fietsen.staat = staat.staatID
-					WHERE fietsID = {$_GET['id']}"
+	$getBikeInfo = "SELECT * FROM fietsen
+INNER JOIN staat
+ON fietsen.staatID = staat.staatID
+INNER JOIN categorieen
+ON fietsen.categorieID = categorieen.categorieID
+WHERE fietsID = {$_GET['id']}";
+	$bikeInfo = $conn->query($getBikeInfo);
+
+	while($row = $bikeInfo->fetch_assoc()) {
 ?>
 	<div id="main">
 		<div id="bikeMain">
-			<img src="Assets/images/dummybike.jpg" alt="fiets">
+			<div id="soldOverlayWrapper">
+				<?php if($row['fietsStatus'] != 'te koop') {?>
+					<div id="soldOverlay">VERKOCHT</div>
+				<?php } ?>
+				<img src="Assets/images/Uploads/<?php echo $row['fietsID']?>.png" alt="fiets">
+			</div>
 			<div id="bikeMainInfoWrapper">
-				<p id="bikeID">Advertentienummer: 001</p>
-				<h2>Lorem Ipsum Dolor Si Di Amet</h2>
-				<p id="bikeState" class="ml-auto">Status: goed</p>
-				<h1>€123,45</h1>
+				<p id="bikeID">Advertentienummer: <?php echo $row['fietsID']; ?></p>
+				<h2><?php echo $row['fietsNaam']?></h2>
+				<p id="bikeState">Staat: <?php echo $row['staatNaam']; ?></p>
+				<h1 class="<?php echo ($row['fietsStatus'] != 'te koop') ? 'soldPrice' : ''; ?>">&euro;<?php echo str_replace(".", ",", $row['fietsPrijs']); ?></h1>
 				<a href="" class="btn btn-primary">Plaats in winkelwagen</a>
 			</div>
 		</div>
 		<div id="bikeDesc">
 			<h1>Omschrijving</h1>
 			<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eu urna libero. Etiam posuere sed dui nec rhoncus. In faucibus tincidunt eleifend. Donec ullamcorper dapibus fringilla. Proin varius dui vel purus faucibus aliquam. Nullam quis lobortis nunc. Sed laoreet diam mauris, hendrerit aliquet ante consequat in. Cras vitae quam rhoncus lectus commodo porta. Maecenas suscipit ante eget elit vestibulum consectetur. Etiam justo massa, dapibus sit amet augue non, egestas facilisis lacus. Aliquam gravida feugiat sem. Mauris lobortis rutrum elit, eget ornare mauris ultrices in.
+			<?php echo $row['fietsBeschrijving']; ?>
 			</p>
 		</div>
 		<div id="bikeSpec">
@@ -31,30 +40,32 @@
 				<table>
 					<tr>
 						<td><b>Merk:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['fietsMerk']; ?></td>
 					</tr>
 					<tr>
 						<td><b>Type:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['fietsModel']?></td>
 					</tr>
 					<tr>
 						<td><b>Framemaat:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['fietsFramemaat']; ?>cm</td>
 					</tr>
+				</table>
+				<table>
 					<tr>
 						<td><b>Categorie:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['categorieNaam']?></td>
 					</tr>
 					<tr>
 						<td><b>Kleur:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['fietsKleur']?></td>
 					</tr>
 					<tr>
 						<td><b>Versnellingen:</b></td>
-						<td>Lorem Ipsum</td>
+						<td> <?php echo $row['fietsAantalVersnellingen']; ?></td>
 					</tr>
 				</table>
 			</div>
 		</div>
 	</div>
-<?php require('Assets/footer.php'); ?>
+<?php } require('Assets/footer.php'); ?>
