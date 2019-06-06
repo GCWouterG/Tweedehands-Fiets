11d188ca2e1422f2dcece5b935650dbbc27f4f6b
yoteth <47322123+yoteth@users.noreply.github.com>
2019-06-06 16:55:17 +0200

    Begin gemaakt

---
+++ profile-page.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link rel="stylesheet" type="text/css" href="Assets/style.css">
+	<link rel="stylesheet" type="text/css" href="Assets/profile-page-style.css">
+</head>
+
+<body>
+	
+	<?php require('Assets/header.php') ?>
+	
+	<div id="profilePageContainer">
+	
+		<main>
+		
+			<div id="leftMenu">
+			
+				<ul>
+					<li>Gegevens</li>
+					<li>Bestellingen</li>
+				</ul>
+				
+			</div>
+		
+		
+		
+		</main>
+	
+	</div>
+	
+	<script src="Assets/script.js"></script>
+	
+</body>
+</html>
